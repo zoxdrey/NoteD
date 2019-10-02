a7bebc8f23b40e4cde939f3ab67ec3aba69dab99
Вячеслав Петров <zoxypva@gmail.com>
2019-10-02 08:08:01 +0300

    Initial commit

---
+++ src/main/java/NoteDServer/controller/UserController.java
@@ -0,0 +1,22 @@
+package NoteDServer.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import NoteDServer.datamodel.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
---
+++ src/main/java/NoteDServer/services/UserService.java
@@ -0,0 +1,4 @@
+package NoteDServer.services;
+
+public class UserService {
+}
